7ae2223f58136a96b1b5627acd9e91cc6f63f825
leshakrot <85496308+leshakrot@users.noreply.github.com>
2024-07-08 19:00:12 +0300

    12.10

---
+++ Assets/Scripts/UI/Damage Text/DamageTextSpawner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace RPG.UI.DamageText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] private DamageText _damageTextPrefab = null;
+
+        void Start()
+        {
+            Spawn(11);
+        }
+
+        public void Spawn(float damageAmount)
+        {
+            DamageText instance = Instantiate<DamageText>(_damageTextPrefab, transform);
+        }
+}
+}
