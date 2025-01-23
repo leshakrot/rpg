92f78a59ce803571606f4b2c12f26a31c964b294
leshakrot <85496308+leshakrot@users.noreply.github.com>
2025-01-23 13:20:31 +0300

    l23

--- Assets/Scripts/UI/Shops/ShopUI.cs
+++ Assets/Scripts/UI/Shops/ShopUI.cs
@@ -1,6 +1,7 @@
 using RPG.Shops;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RPG.UI.Shops
 {
@@ -10,15 +11,20 @@ namespace RPG.UI.Shops
         [SerializeField] Transform listRoot;
         [SerializeField] RowUI rowPrefab;
         [SerializeField] TextMeshProUGUI totalField;
+        [SerializeField] Button confirmButton;
 
         Shopper shopper = null;
         Shop currentShop = null;
+
+        Color originalTotalTextColor;
         private void Start()
         {
+            originalTotalTextColor = totalField.color;
             shopper = GameObject.FindGameObjectWithTag("Player").GetComponent<Shopper>();
             if (shopper == null) return;
 
             shopper.activeShopChange += ShopChanged;
+            confirmButton.onClick.AddListener(ConfirmTransaction);
 
             ShopChanged();
         }
@@ -54,6 +60,8 @@ namespace RPG.UI.Shops
             }
 
             totalField.text = $"Total: ${currentShop.TransactionTotal():N2}";
+            totalField.color = currentShop.HasSufficientFunds() ? originalTotalTextColor : Color.red;
+            confirmButton.interactable = currentShop.CanTransact();
         }
 
         public void Close()
