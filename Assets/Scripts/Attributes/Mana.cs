29a57d95cd85faf7b16eebcd6c01a57c343a4906
leshakrot <85496308+leshakrot@users.noreply.github.com>
2025-03-16 15:08:03 +0300

    lesson 16

--- Assets/Scripts/Attributes/Mana.cs
+++ Assets/Scripts/Attributes/Mana.cs
@@ -1,36 +1,53 @@
+using GameDevTV.Utils;
+using RPG.Stats;
 using UnityEngine;
 
 namespace RPG.Attributes
 {
     public class Mana : MonoBehaviour
     {
-        [SerializeField] float maxMana = 200f;
-
-        float mana;
+        LazyValue<float> mana;
 
         private void Awake()
         {
-            mana = maxMana;
+            mana = new LazyValue<float>(GetMaxMana);
+        }
+
+        private void Update()
+        {
+            if(mana.value < GetMaxMana())
+            {
+                mana.value += GetRegenRate() * Time.deltaTime;
+                if(mana.value > GetMaxMana())
+                {
+                    mana.value = GetMaxMana();
+                }
+            }
         }
 
         public float GetMana()
         {
-            return mana;
+            return mana.value;
         }
 
         public float GetMaxMana()
         {
-            return maxMana;
+            return GetComponent<BaseStats>().GetStat(Stat.Mana);
+        }
+
+        public float GetRegenRate()
+        {
+            return GetComponent<BaseStats>().GetStat(Stat.ManaRegenRate);
         }
 
         public bool UseMana(float manaToUse)
         {
-            if(manaToUse > mana)
+            if(manaToUse > mana.value)
             {
                 return false;
             }
 
-            mana -= manaToUse;
+            mana.value -= manaToUse;
             return true;
         }
     }
