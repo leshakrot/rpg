9057ba345d5eff44b16e30f1c354b7631248571f
leshakrot <85496308+leshakrot@users.noreply.github.com>
2025-03-17 10:39:20 +0300

    staging and commiting traits

---
+++ Assets/Scripts/UI/TraitUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using RPG.Stats;
+
+namespace RPG.UI
+{
+	public class TraitUI : MonoBehaviour
+	{
+		[SerializeField] TextMeshProUGUI unassignedPointsText;
+		[SerializeField] Button commitButton;
+		
+		TraitStore playerTraitStore = null;
+
+		private void Start() 
+		{
+			playerTraitStore = GameObject.FindGameObjectWithTag("Player").GetComponent<TraitStore>();
+			commitButton.onClick.AddListener(playerTraitStore.Commit);
+		}
+		
+		private void Update()
+		{
+			unassignedPointsText.text = playerTraitStore.GetUnassignedPoints().ToString();
+		}
+	}
+}
