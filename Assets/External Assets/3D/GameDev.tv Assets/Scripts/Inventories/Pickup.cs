5912144cb7918a210bf1276503c9dc931a614ef9
leshakrot <85496308+leshakrot@users.noreply.github.com>
2025-04-30 15:43:42 +0300

    upd pickup to actionstore

--- Assets/External Assets/3D/GameDev.tv Assets/Scripts/Inventories/Pickup.cs
+++ Assets/External Assets/3D/GameDev.tv Assets/Scripts/Inventories/Pickup.cs
@@ -14,6 +14,7 @@ namespace GameDevTV.Inventories
 
         // CACHED REFERENCE
         Inventory inventory;
+        ActionStore actionStore;
 
         // LIFECYCLE METHODS
 
@@ -21,6 +22,7 @@ namespace GameDevTV.Inventories
         {
             var player = GameObject.FindGameObjectWithTag("Player");
             inventory = player.GetComponent<Inventory>();
+            actionStore = player.GetComponent<ActionStore>();
         }
 
         // PUBLIC
@@ -53,12 +55,39 @@ namespace GameDevTV.Inventories
 	    public void PickupItem()
 	    {
 		    Debug.Log($"[Pickup] Поднятие предмета: {item?.name}, количество: {number}");
-
-		    bool foundSlot = inventory.AddToFirstEmptySlot(item, number);
-		    if (foundSlot)
+		    
+		    // Проверяем, является ли предмет ActionItem и есть ли он уже в ActionStore
+		    bool addedToActionSlot = false;
+		    if (item is ActionItem)
 		    {
-			    Destroy(gameObject);
+		        // Проходимся по всем слотам ActionStore
+		        for (int i = 0; i < 8; i++) // Предполагаем, что ActionStore имеет 8 слотов
+		        {
+		            ActionItem actionItem = actionStore.GetAction(i);
+		            
+		            // Если нашли такой же предмет в ActionStore
+		            if (object.ReferenceEquals(actionItem, item))
+		            {
+		                Debug.Log($"[Pickup] Добавление предмета {item.name} в ActionSlot {i}");
+		                actionStore.AddAction(item, i, number);
+		                addedToActionSlot = true;
+		                break;
+		            }
+		        }
 		    }
+		    
+		    // Если предмет не был добавлен в ActionSlot, добавляем его в инвентарь
+		    if (!addedToActionSlot)
+		    {
+		        bool foundSlot = inventory.AddToFirstEmptySlot(item, number);
+		        if (!foundSlot)
+		        {
+		            return; // не уничтожаем объект, если нет места в инвентаре
+		        }
+		    }
+		    
+		    // Уничтожаем объект после успешного подбора
+		    Destroy(gameObject);
 	    }
 
         public bool CanBePickedUp()
