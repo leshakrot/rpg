d5a9bd74750b151563c8364a079b27bb898f2f9b
leshakrot <alexeiikrot@gmail.com>
2026-05-08 21:05:10 +0300

    upd3

--- Assets/Scripts/TriggeredQuestObject.cs
+++ Assets/Scripts/TriggeredQuestObject.cs
@@ -1,41 +1,66 @@
-﻿using UnityEngine;
+using UnityEngine;
 using RPG.Control;
 using GameDevTV.Saving;
 
 public class TriggeredQuestObject : MonoBehaviour, ISaveable
 {
-	private bool _isTriggered;
+    private bool _isTriggered;
+    
+    // Триггер игнорирует физические пересечения до конца первого кадра.
+    // Это защищает от ложных срабатываний в WebGL, где физика может
+    // зафиксировать коллизию в момент расстановки объектов при старте сцены.
+    private bool _isReady;
 
-	private void Start()
-	{
-		if(_isTriggered) SelfDestroy();
-	}
-	
-	private void OnTriggerEnter(Collider other)
-	{
-		if(other.TryGetComponent(out PlayerController player) && !_isTriggered)
-		{
-			_isTriggered = true; 
-		}           
-	}
-
-	public void SetTriggered()
+    private void Start()
     {
-		_isTriggered = true;
-	}
-	
-	public void SelfDestroy()
-	{
-		Destroy(gameObject);
-	}
+        if (_isTriggered)
+        {
+            SelfDestroy();
+            return;
+        }
+        
+        // Откладываем активацию триггера на один физический кадр
+        Invoke(nameof(SetReady), Time.fixedDeltaTime);
+    }
 
-	public object CaptureState()
-	{
-		return _isTriggered;
-	}
+    private void SetReady()
+    {
+        _isReady = true;
+    }
 
-	public void RestoreState(object state)
-	{
-		_isTriggered = (bool)state;
-	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!_isReady) return;
+        
+        if (other.TryGetComponent(out PlayerController player) && !_isTriggered)
+        {
+            _isTriggered = true;
+        }
+    }
+
+    public void SetTriggered()
+    {
+        _isTriggered = true;
+    }
+
+    public void SelfDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+    public object CaptureState()
+    {
+        return _isTriggered;
+    }
+
+    public void RestoreState(object state)
+    {
+        _isTriggered = (bool)state;
+        // Start() мог уже отработать к этому моменту (особенно в WebGL).
+        // Поэтому проверяем и здесь.
+        if (_isTriggered)
+        {
+            SelfDestroy();
+        }
+    }
 }
